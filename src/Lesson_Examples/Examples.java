b8c64f334e1ffe3d0842ba20dfad96a58e2974ed
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-04-22 16:20:40 -0400

    Add files via upload

---
+++ src/Lesson_Examples/Examples.java
@@ -0,0 +1,18 @@
+package Lesson_Examples;
+
+public class Examples {
+
+	public static void main(String[] args) {
+		//Create a Student object
+		Student s = new Student();
+		//Set the grade and gpa
+		s.setGpa(3.9);
+		s.setSchedule("Band, Computer Science, Art");
+		//Set the inherited name and birthday
+		s.setName("Molly");
+		s.setBirthday("September 15, 2010");
+		
+		System.out.println(s.getName() + " takes " + s.getSchedule());
+	}
+
+}
